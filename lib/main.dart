7e5f775ee6cf41007c94bfd926f0bc0b15673ce8
Roi-Livne <roilivne14@gmail.com>
2021-06-22 07:38:38 +0300

    final ex fin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -26,11 +27,22 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Center(
-      child: Image.asset('./images/ball1.png'),
+      child: TextButton(
+          onPressed: () {
+            changeBallNumber();
+          },
+          child: Image.asset('./images/ball$ballNumber.png')),
     ));
   }
 }
